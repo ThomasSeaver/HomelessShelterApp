05547f98754952ee4f55b40cea439c3cde90ab67
obecerra3 <obecerra3@gatech.edu>
2018-02-19 22:06:22 -0500

    welcome_screen fixed up and loginActivity started

--- app/src/main/java/org/team69/homelessshelterapp/controllers/LoginActivity.java
+++ app/src/main/java/org/team69/homelessshelterapp/controllers/LoginActivity.java
@@ -1,11 +1,29 @@
 package org.team69.homelessshelterapp.controllers;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Button;
+
+import org.team69.homelessshelterapp.R;
 
 /**
  * Created by obecerra on 2/19/18.
  */
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button doneButton;
+    private Button cancelButton;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+
+
+    }
 }
