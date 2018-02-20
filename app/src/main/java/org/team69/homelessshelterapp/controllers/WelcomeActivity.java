7337edc966c08ab74ab894b864962be94bd337f7
tseaver6 <dancerfool@gmail.com>
2018-02-19 21:50:04 -0500

    Login screen added

--- app/src/main/java/org/team69/homelessshelterapp/controllers/WelcomeActivity.java
+++ app/src/main/java/org/team69/homelessshelterapp/controllers/WelcomeActivity.java
@@ -1,11 +1,20 @@
 package org.team69.homelessshelterapp.controllers;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import org.team69.homelessshelterapp.R;
 
 /**
  * Created by obecerra on 2/19/18.
  */
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_screen);
+    }
+
 }
