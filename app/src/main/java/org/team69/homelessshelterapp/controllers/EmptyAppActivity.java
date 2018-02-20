76751cd86dd355622136c1c133871694d88787ba
obecerra3 <obecerra3@gatech.edu>
2018-02-20 14:29:12 -0500

    finished activities

--- app/src/main/java/org/team69/homelessshelterapp/controllers/EmptyAppActivity.java
+++ app/src/main/java/org/team69/homelessshelterapp/controllers/EmptyAppActivity.java
@@ -1,7 +1,12 @@
 package org.team69.homelessshelterapp.controllers;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import org.team69.homelessshelterapp.R;
 
 /**
  * Created by obecerra on 2/19/18.
@@ -9,4 +14,22 @@ import android.support.v7.app.AppCompatActivity;
 
 public class EmptyAppActivity extends AppCompatActivity {
 
+    private Button logoutButton;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_screen);
+
+        logoutButton =  findViewById(R.id.button5);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                goToLogin();
+            }
+        });
+    }
+
+    private void goToLogin() {
+        Intent intent = new Intent(getBaseContext(), WelcomeActivity.class);
+        startActivity(intent);
+    }
 }
