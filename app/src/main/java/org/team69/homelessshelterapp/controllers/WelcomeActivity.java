a1455175447b4cfa91a23579fd8de1c3212631c5
obecerra3 <obecerra3@gatech.edu>
2018-02-19 20:36:48 -0500

    Organizing controller and model packages, adding empty activities to be implemented for M4

--- app/src/main/java/org/team69/homelessshelterapp/controllers/WelcomeActivity.java
+++ app/src/main/java/org/team69/homelessshelterapp/controllers/WelcomeActivity.java
@@ -1,8 +1,11 @@
 package org.team69.homelessshelterapp.controllers;
 
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by obecerra on 2/19/18.
  */
 
-public class WelcomeActivity {
+public class WelcomeActivity extends AppCompatActivity {
 }
