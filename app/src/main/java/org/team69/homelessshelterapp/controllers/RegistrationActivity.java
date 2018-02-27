7a9ee9ef3bca4f321bd3d1273d32166ed7afdd22
obecerra3 <obecerra3@gatech.edu>
2018-02-26 21:47:32 -0500

    registration activity in progress

--- app/src/main/java/org/team69/homelessshelterapp/controllers/RegistrationActivity.java
+++ app/src/main/java/org/team69/homelessshelterapp/controllers/RegistrationActivity.java
@@ -4,11 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Button;
 import android.view.View;
 import android.widget.Spinner;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
 
 import org.team69.homelessshelterapp.R;
 import org.team69.homelessshelterapp.model.UserPassMap;
@@ -22,8 +28,7 @@ public class RegistrationActivity extends AppCompatActivity {
     private Button cancelButton;
     private EditText usernameInput;
     private EditText passwordInput;
-    private Spinner adminOrUser;
-    private EditText wrongLogin;
+    private Spinner adminOrUserSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,34 +36,50 @@ public class RegistrationActivity extends AppCompatActivity {
         setContentView(R.layout.login_screen);
 
 
-        doneButton =  findViewById(R.id.button3);
+        doneButton =  findViewById(R.id.RegistrationDoneButton);
         doneButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                checkUserPass();
+                createUser();
             }
         });
 
-        cancelButton = findViewById(R.id.button4);
+        cancelButton = findViewById(R.id.RegistrationCancelButton);
         cancelButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 goBackToWelcome();
             }
         });
 
-        usernameInput = (EditText) findViewById(R.id.userBox);
-        passwordInput = (EditText) findViewById(R.id.passBox);
-        wrongLogin = (EditText) findViewById(R.id.wrongLoginText);
+        usernameInput = (EditText) findViewById(R.id.RegistrationUsernameText);
+        passwordInput = (EditText) findViewById(R.id.RegistrationPasswordText);
 
+        adminOrUserSpinner = (Spinner) findViewById(R.id.AdminOrUserSpinner);
+        String[] possibleValues = new String[2];
+        possibleValues[0] = "User";
+        possibleValues[1] = "Admin";
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, possibleValues);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adminOrUserSpinner.setAdapter(adapter);
     }
 
-    private void checkUserPass() {
-        if (usernameInput.getText().toString().equals("user") &&
-                passwordInput.getText().toString().equals("pass")) {
-            wrongLogin.setVisibility(View.INVISIBLE);
-            Intent intent = new Intent(getBaseContext(), EmptyAppActivity.class);
-            startActivity(intent);
-        } else {
-            wrongLogin.setVisibility(View.VISIBLE);
+    private void createUser() {
+        try {
+            FileInputStream fileInputStream = new FileInputStream("theUserPassMap.ser");
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            UserPassMap myNewlyReadInMap = (UserPassMap) objectInputStream.readObject();
+            objectInputStream.close();
+
+            myNewlyReadInMap.addTo(usernameInput.getText().toString(), passwordInput.getText().toString());
+
+            FileOutputStream fileOutputStream = new FileOutputStream("myMap.whateverExtension");
+            ObjectOutputStream objectOutputStream= new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(myNewlyReadInMap);
+            objectOutputStream.close();
+
+        } catch (IOException i) {
+            return;
+        } catch (ClassNotFoundException c) {
+            return;
         }
     }
 
